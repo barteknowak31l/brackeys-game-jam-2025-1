1c09b9750327f2111559e53fcc1ae116cb71ef0c
Igor <137097794+qtatv2@users.noreply.github.com>
2025-02-18 21:03:00 +0100

    Added font

--- Assets/Scripts/Message/MessagesManager.cs
+++ Assets/Scripts/Message/MessagesManager.cs
@@ -8,7 +8,9 @@ public class MessagesManager : MonoBehaviour
     private Dictionary<DictKey, string> billboardMessages = new Dictionary<DictKey, string>()
     {
         { new DictKey(States.Wind, Variant.First), "The wind is picking up" },
+        { new DictKey(States.Wind, Variant.Second), "The wind is picking up" },
         { new DictKey(States.Anvil, Variant.First), "Look up!" },
+        { new DictKey(States.Anvil, Variant.Second), "Look up!" },
         { new DictKey(States.Storm, Variant.First), "Storm is coming... it will be slippery" },
         { new DictKey(States.Storm, Variant.Second), "Thunder!" },
         { new DictKey(States.Ufo, Variant.First), "Mother ship incoming" },

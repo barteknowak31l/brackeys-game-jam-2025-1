52288a75bb1f1745d784cc78a481156357717ade
Mateusz <rozumpl2@gmail.com>
2025-02-19 17:40:49 +0100

    Add moving ship

--- Assets/Scripts/Ship/CannonBall.cs
+++ Assets/Scripts/Ship/CannonBall.cs
@@ -1,9 +1,15 @@
+using StateMachine.states;
 using UnityEngine;
 
 public class CannonBall : MonoBehaviour
 {
     public float lifeTime = 5f;
+    FruitState _state;
 
+    public void Setup(FruitState state)
+    {
+        _state = state;
+    }
     void Start()
     {
         Destroy(gameObject, lifeTime); 
--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -1,40 +1,88 @@
+using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
+using StateMachine.states;
 using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
-    public Transform firePoint;
-    public float fireForce = 10f;
-    public float angle = 45f;
-    public float rotation = 180f;
-    public float shootInterval = 2f;
-    private float shootTimer = 0f;
-    public List<GameObject> _instantiatedFruit;
-    private void Start()
-    {
-        
+    [SerializeField] private float _fireForce = 10f;
+    [SerializeField] private float _angle = 45f;
+    [SerializeField] private float _rotation = 180f;
+    [SerializeField] private float _shootInterval = 1.5f;
+    [SerializeField] private float _shootTimer = 0f;
+    [SerializeField] private List<GameObject> _fruits;
+    [SerializeField] private List<Transform> _fireFruitPoints;
 
+    [SerializeField] private FruitState _state;
+
+    public void Setup(FruitState state)
+    {
+        _state = state;
 
     }
+
     void Update()
     {
-        shootTimer += Time.deltaTime;
-        if (shootTimer >= shootInterval)
         {
-            Shoot();
-            shootTimer = 0f;
+            _shootTimer += Time.deltaTime;
+            if (_shootTimer >= _shootInterval)
+            {
+                Shoot();
+                _shootTimer = 0f;
+            }
         }
     }
+    public void MovePhase(Vector3 targetPosition, float speed)
+    {
 
+        StartCoroutine(MoveToPosition(targetPosition, speed));
+    }
+    IEnumerator MoveToPosition(Vector3 targetPosition, float speed)
+    {
+        Vector3 startPosition = transform.position;
+        float distance = Vector3.Distance(startPosition, targetPosition);
+        float duration = distance / speed;
+
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        FinishMovement();
+    }
+    private void FinishMovement()
+    {
+        StopAllCoroutines();
+        _state.EnterStationaryPhase();
+    }
+    public void StationaryPhase(float duration)
+    {
+        StartCoroutine(StationaryPhaseCoroutine(duration));
+    }
+    IEnumerator StationaryPhaseCoroutine(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        FinishStationaryPhase();
+    }
+    private void FinishStationaryPhase()
+    {
+        StopAllCoroutines();
+        _state.EnterMovementPhase();
+    }
     void Shoot()
     {
-        var rot = Quaternion.Euler(rotation, 0, 0);
-        GameObject bullet = Instantiate(_instantiatedFruit[Random.Range(0,_instantiatedFruit.Count)], firePoint.position, rot);
+        var rot = Quaternion.Euler(_rotation, 0, 0);
+        GameObject bullet = Instantiate(_fruits[Random.Range(0, _fruits.Count)], _fireFruitPoints[Random.Range(0,_fireFruitPoints.Count)].position, rot);
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
 
         
-        Vector3 shootDirection = Quaternion.AngleAxis(angle, transform.right) * transform.forward;
-        rb.linearVelocity = shootDirection * fireForce;
+        Vector3 shootDirection = Quaternion.AngleAxis(_angle, transform.right) * transform.forward;
+        rb.linearVelocity = shootDirection * _fireForce;
     }
 }

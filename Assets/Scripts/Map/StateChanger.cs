f295d970f0509e0edeeb246bbc47971aee352687
Mateusz <rozumpl2@gmail.com>
2025-02-20 18:45:15 +0100

    Add stage changer

---
+++ Assets/Scripts/Map/StateChanger.cs
@@ -0,0 +1,28 @@
+using StateMachine;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StateChanger : MonoBehaviour
+{
+    private Collider _collider;
+    [SerializeField] private StateMachineManager _stateMachineBehaviour;
+    private bool _active = false;
+    void Start()
+    {
+        _collider = GetComponent<Collider>();
+    }
+    private void OnEnable()
+    {
+        _active = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && _active == false)
+        {
+            _stateMachineBehaviour.EndMechanicState();
+            _active = true;
+        }
+    }
+}
+

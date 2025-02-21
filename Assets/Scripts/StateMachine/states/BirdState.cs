2cc16393848d3aa21b4b1b8cb4808bd1cbe2f049
Mateusz <rozumpl2@gmail.com>
2025-02-21 22:42:18 +0100

    BirdStado

--- Assets/Scripts/StateMachine/states/BirdState.cs
+++ Assets/Scripts/StateMachine/states/BirdState.cs
@@ -31,7 +31,6 @@ namespace StateMachine.states
         [SerializeField] private GameObject _littleBirdPrefab;
         [SerializeField] private GameObject _bigBirdPrefab;
 
-        private List<GameObject> _instantiatedBird;
 
         public void EnterState(StateMachineManager ctx)
         {
@@ -65,7 +64,7 @@ namespace StateMachine.states
 
         }
 
-        private void SpawnBird()
+        IEnumerator SpawnBird()
         {
             _birdSpawnDistance *= Random.Range(0.9f, 1.1f);
             Vector3 position = new Vector3(_playerTransform.position.x, _birdYOffset, _playerTransform.position.z) + _playerTransform.forward * _birdSpawnDistance;
@@ -74,16 +73,19 @@ namespace StateMachine.states
             position += _playerTransform.right * side * _birdSideOffset;
             for (int i = 0; i < _birdCount; i++)
             {
-
+                float spawnDelay = Random.Range(1f, 1.5f);
 
                 var prefab = _variant == Variant.First ? _littleBirdPrefab : _bigBirdPrefab;
 
                 var rotation = side == -1f ? Quaternion.Euler(0, 0, 0) : Quaternion.Euler(0, 180, 0);
 
+                float birdXOffset = 2f;
+                position = position + new Vector3(i+ birdXOffset, 0f, 0f);
 
 
                 GameObject bird = Instantiate(prefab, position, rotation);
                 bird.GetComponent<Bird>().Setup(this);
+                yield return new WaitForSeconds(spawnDelay);
             }
 
         }
@@ -102,7 +104,7 @@ namespace StateMachine.states
             {
                 var spawnTime = _birdSpawnTimeBase + Random.Range(-_birdSpawnTimeOffset, _birdSpawnTimeOffset);
                 yield return new WaitForSeconds(spawnTime);
-                SpawnBird();
+                StartCoroutine(SpawnBird());
             }
         }
     }

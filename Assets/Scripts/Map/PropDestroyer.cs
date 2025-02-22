f96eaf3217a5a1697e56428f93ef2722e01f81af
Mateusz <rozumpl2@gmail.com>
2025-02-22 15:20:41 +0100

    MainScene fix

--- Assets/Scripts/Map/PropDestroyer.cs
+++ Assets/Scripts/Map/PropDestroyer.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class PropDestroyer : MonoBehaviour
 {
-
     private void OnCollisionEnter(Collision collision)
     {
-        Destroy(collision.gameObject);
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Prop"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
+
